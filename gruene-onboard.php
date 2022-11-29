b5feb671423f082e9919e4c4d4f70eb2445f899f
Cyrill Bolliger <mail@cyrill.me>
2022-11-29 16:00:18 +0100

    [RELEASE] new version number

--- gruene-onboard.php
+++ gruene-onboard.php
@@ -6,7 +6,7 @@
  * Author:          grueneschweiz
  * Author URI:      https://gruene.ch
  * Text Domain:     gruene-onboard
- * Version:         1.3.2
+ * Version:         1.4.0
  *
  * @package         Gruene_Onboard
  */
