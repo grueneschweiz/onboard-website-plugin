74f549a67753cc948d8453e33b5e024b12506edc
Sandro Tanner <sandro.tanner@gruene.ch>
2023-06-15 14:38:43 +0200

    [RELEASE] new version number

--- gruene-onboard.php
+++ gruene-onboard.php
@@ -6,7 +6,7 @@
  * Author:          grueneschweiz
  * Author URI:      https://gruene.ch
  * Text Domain:     gruene-onboard
- * Version:         1.4.0
+ * Version:         1.4.1
  *
  * @package         Gruene_Onboard
  */
